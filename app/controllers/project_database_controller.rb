a2a0bb80686c07cfe0dff26122368afae66c5e86
Seachaos <ye.jian.yin@gmail.com>
2016-08-15 02:23:24 +0800

    let table can be move

--- app/controllers/project_database_controller.rb
+++ app/controllers/project_database_controller.rb
@@ -46,7 +46,37 @@ class ProjectDatabaseController < ApplicationController
 		}
 	end
 
+	def api_save_table_position
+		return unless verify_table_for_api
+		@table.x = params[:x].to_i
+		@table.y = params[:y].to_i
+		@table.save
+
+		render :json => {
+			:status => 0
+		}
+	end
+
 protected
+
+	def verify_table_for_api
+		@table = TableHeader.find_by_id(params[:table_id])
+		unless @table.present? then
+			render :json => {
+				:status => 1,
+				:emsg => 'wrong table'
+			}
+			return false
+		end
+		unless @table.project_id == @project.id then
+			render :json => {
+				:status => 1,
+				:emsg => 'wrong table'
+			}
+			return false
+		end
+		return true
+	end
 
 	def _project_select
 		unless session[:project_id].present?
